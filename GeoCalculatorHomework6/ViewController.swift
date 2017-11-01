1ef0f51722ef0e36607526ad5bab745d0af338bc
LenoXMasonJAR <mahonema5@gmail.com>
2017-11-01 17:16:54 -0400

    Initial Commit

---
+++ GeoCalculatorHomework6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoCalculatorHomework6
+//
+//  Created by Mason Mahoney on 11/1/17.
+//  Copyright © 2017 Mason Mahoney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
